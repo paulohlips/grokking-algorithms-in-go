77923500defc5d1914ca60f6df372cc53d1f7273
paulohlips <paulohfcmendes@gmail.com>
2023-10-21 14:14:13 -0300

    feat: creates simple sort function

--- cap2-simple-sort/simple-sort_test.go
+++ cap2-simple-sort/simple-sort_test.go
@@ -1,10 +1,12 @@
 package cap2simplesort
 
 import (
+	"reflect"
 	"testing"
 )
 
-/* func TestSort(t *testing.T) {
+func TestSort(t *testing.T) {
+	t.Helper()
 	t.Run("sort list", func(t *testing.T) {
 		list := []int{3, 1, 7, 0}
 
@@ -16,14 +18,17 @@ import (
 		}
 	})
 }
-*/
+
 func TestFindSmaller(t *testing.T) {
-	list := []int{7, 2, 0, 8, 1}
+	t.Helper()
+	t.Run("find smaller", func(t *testing.T) {
+		list := []int{7, 2, 0, 8, 1}
 
-	got := FindSmaller(list)
-	want := 0
+		got := FindSmaller(list)
+		want := []int{0, 2}
 
-	if got != want {
-		t.Errorf("got %d but want %d", got, want)
-	}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
 }
--- cap2-simple-sort/simples-sort.go
+++ cap2-simple-sort/simples-sort.go
@@ -2,27 +2,35 @@ package cap2simplesort
 
 import "fmt"
 
+func FindSmaller(list []int) []int {
+		smaller := list[0]
+		index := 0
+
+		for i, item := range list {
+			if smaller > item {
+				smaller = item
+				index = i
+			}
+		}
+
+	return []int{smaller, index}
+}
+
 func Sort(list []int) []int {
 	//7, 2, 0, 8, 1
 	sortedList := make([]int, len(list))
 
-	// smaller := list[0]
+	fmt.Println(sortedList)
 
 	for i := range list {
-		fmt.Println(i)
+		result := FindSmaller(list)
+
+		smaller := result[0]
+		index := result[1]
+
+		sortedList[i] = smaller
+		list = append(list[:index], list[index+1:]...)
 	}
 
 	return sortedList
-}
-
-func FindSmaller(list []int) int {
-		smaller := list[0]
-
-		for _, item := range list {
-			if smaller > item {
-				smaller = item
-			}
-		}
-
-	return smaller
 }
