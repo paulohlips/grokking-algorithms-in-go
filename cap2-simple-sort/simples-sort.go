c0988f26edac7aca8e86c4b01686f0037aef5f59
paulohlips <paulohfcmendes@gmail.com>
2023-10-21 13:46:21 -0300

    fix: fixes FindSmaller function

--- cap2-simple-sort/simples-sort.go
+++ cap2-simple-sort/simples-sort.go
@@ -1,11 +1,28 @@
 package cap2simplesort
 
+import "fmt"
+
 func Sort(list []int) []int {
+	//7, 2, 0, 8, 1
 	sortedList := make([]int, len(list))
+
+	// smaller := list[0]
+
+	for i := range list {
+		fmt.Println(i)
+	}
 
 	return sortedList
 }
 
 func FindSmaller(list []int) int {
-	return 0
+		smaller := list[0]
+
+		for _, item := range list {
+			if smaller > item {
+				smaller = item
+			}
+		}
+
+	return smaller
 }
