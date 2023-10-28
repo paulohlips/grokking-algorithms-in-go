1f696e7315ed1f7fa3ad59e1d982ad913ca7a452
paulohlips <paulohfcmendes@gmail.com>
2023-10-28 13:58:30 -0300

    feat: bfs working

--- cap6-breadth-first-search/bfs.go
+++ cap6-breadth-first-search/bfs.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Stack map[string][]string
 
 func main() {
-	stack := Stack{"js": {"node", "react"}}
-	stack["java"] = append(stack["java"], "spring")
+	stack := Stack{"paul": {"james", "bruce", "clark"}}
+	stack["james"] = append(stack["james"], "bond")
+	stack["bruce"] = append(stack["bruce"], "banner")
+	stack["bond"] = append(stack["bond"], "007")
 
-	fmt.Printf("%s\n", stack["java"][0])
+	/*
+		paul --- james --- bond
+		 |
+		 |----- bruce --- banner
+		 |----- clark
+	*/
+
+	queue := stack["paul"]
+
+	for {
+		fmt.Printf("fila: %v \n", queue)
+		if "007" == queue[0] {
+			fmt.Print("nome encontrado!")
+			return
+		}
+		fmt.Printf("nome não é %s!\n", queue[0])
+		queue = append(queue, stack[queue[0]]...)
+		queue = queue[1:]
+		fmt.Printf("nova fila: %v \n\n\n", queue)
+	}
 }
